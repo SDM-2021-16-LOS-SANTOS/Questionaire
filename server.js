d94036af74945d1aa11da4736c52229bd0da86fc
BUPE9999 <83247710+BUPE9999@users.noreply.github.com>
2021-10-24 22:07:58 +0200

    temporary server almost done

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const http = require("http");
-
-const server =  http.createServer((request, Response) => {
-    Response.end("hello from NodeJS Server 23");
-});
-
-const port = 2000;
+const fs = require("fs");
+const port = 3000;
 const ip = "127.0.0.1";
 
+const sendResponse = (filename, statusCode, response) => {
+    fs.readFile(`./${filename}`, (error, data) => {
+        if(error){
+            response.statusCode = 500;
+            response.setHeader("Content-Type","text/plain");
+            response.end("ERROR!");
+        } else{
+            response.statusCode = statusCode;
+            response.setHeader("Content-Type","text/html");
+            response.end(data);
+        }
+    });
+};
 
-server.listen(port,ip,()=>{
+const server =  http.createServer((request, response) => {
+    // console.log(request.url, request.method);
+    const method = request.method;
+    let url = request.url;
+
+
+    if(method === "GET") {
+        sendResponse("Questionaire.html",200,response);
+    }
+    else{
+        sendResponse("404.html",404,response);
+
+    }     
+});
+
+
+server.listen(port,ip,() => {
     console.log(`Server is running at http://${ip}:${port}`);
 });
